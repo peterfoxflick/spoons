3f63e4d77120a453b1bf9b5f5dd7674037d3b71b
peterfoxflick <peterfoxflick@gmail.com>
2018-11-24 17:27:45 -0700

    testing with all in routers

--- index.js
+++ index.js
@@ -117,3 +117,122 @@ function getPersonFromDb(id, callback) {
 	});
 
 } // end of getPersonFromDb
+
+
+
+//CREATE
+function locationCreateDB(name, callback) {
+  //Check that name is string and min length
+  if ((typeof name) == "string"){
+    var sql = "INSERT INTO location (name) VALUE $1::string";
+
+    // We now set up an array of all the parameters we will pass to fill the
+    // placeholder spots we left in the query.
+    var params = [name];
+
+    // This runs the query, and then calls the provided anonymous callback function
+    // with the results.
+    pool.query(sql, params, function(err, result) {
+      // If an error occurred...
+      if (err) {
+        console.log("Error in query: ")
+        console.log(err);
+        callback(err, null);
+      }
+
+      // Log this to the console for debugging purposes.
+      console.log("Create new location: " + JSON.stringify(result.rows));
+
+
+      // When someone else called this function, they supplied the function
+      // they wanted called when we were all done. Call that function now
+      // and pass it the results.
+
+      // (The first parameter is the error variable, so we will pass null.)
+      callback(null, result.rows);
+    });
+  }
+}
+
+//READ
+function locationGetDB(id, callback) {
+  //Check that name is string and min length
+  if ((typeof id) == "number"){
+    var sql = "SELECT * FROM member_copy WHERE id = $1::int";
+
+    // We now set up an array of all the parameters we will pass to fill the
+    // placeholder spots we left in the query.
+    var params = [id];
+
+    // This runs the query, and then calls the provided anonymous callback function
+    // with the results.
+    pool.query(sql, params, function(err, result) {
+      // If an error occurred...
+      if (err) {
+        console.log("Error in query: ")
+        console.log(err);
+        callback(err, null);
+      }
+
+      // Log this to the console for debugging purposes.
+      console.log("Found location: " + JSON.stringify(result.rows[0]));
+
+
+      // When someone else called this function, they supplied the function
+      // they wanted called when we were all done. Call that function now
+      // and pass it the results.
+
+      // (The first parameter is the error variable, so we will pass null.)
+      callback(null, result.rows[0]);
+    });
+  }
+}
+
+}
+
+function createLocation(request, response) {
+	// First get the person's id
+	var name = request.query.name;
+
+	// TODO: We should really check here for a valid id before continuing on...
+
+	// use a helper function to query the DB, and provide a callback for when it's done
+	locationCreateDB(name, function(error, result) {
+		// This is the callback function that will be called when the DB is done.
+		// The job here is just to send it back.
+
+		// Make sure we got a row with the person, then prepare JSON to send back
+		if (error || result == null || result.length != 1) {
+			response.status(500).json({success: false, data: error});
+		} else {
+			var person = result[0];
+			response.status(200).json(result[0]);
+		}
+	});
+}
+
+
+function getLocation(request, response) {
+	// First get the person's id
+	var id = request.query.id;
+
+	// TODO: We should really check here for a valid id before continuing on...
+
+	// use a helper function to query the DB, and provide a callback for when it's done
+	locationGetDB(id, function(error, result) {
+		// This is the callback function that will be called when the DB is done.
+		// The job here is just to send it back.
+
+		// Make sure we got a row with the person, then prepare JSON to send back
+		if (error || result == null || result.length != 1) {
+			response.status(500).json({success: false, data: error});
+		} else {
+			var person = result[0];
+			response.status(200).json(result[0]);
+		}
+	});
+}
+
+
+
+//DESTROY
