96648d5fe2fc55095d8abfb515f58e9ac79a115d
peterfoxflick <peterfoxflick@gmail.com>
2018-11-27 15:16:33 -0700

    Working on start function

--- controllers/enroll.js
+++ controllers/enroll.js
@@ -58,7 +58,7 @@ exports.get = function(request, response, pool) {
 				if (error || result == null) {
 					response.status(500).json({success: false, data: error});
 				} else {
-					response.status(200).json(result[0]);
+					response.status(200).json(result);
 				}
 			});
 		}
