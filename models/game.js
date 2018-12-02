5ee2333067158d2e13fe83d5b9736554e9303d74
peterfoxflick <peterfoxflick@gmail.com>
2018-12-01 19:03:32 -0700

    fetching enrollment data

--- models/game.js
+++ models/game.js
@@ -52,6 +52,31 @@ exports.get = function(id, pool, callback) {
       });
   }
 
+  //READ
+  exports.getAllWithUserId = function(id, pool, callback) {
+        var sql = "SELECT * FROM game INNER JOIN enrollment on enrollment.game_id = game.id WHERE enrollment.user_id = $1;";
+
+        // We now set up an array of all the parameters we will pass to fill the
+        // placeholder spots we left in the query.
+        var params = [id];
+
+        // This runs the query, and then calls the provided anonymous callback function
+        // with the results.
+        pool.query(sql, params, function(err, result) {
+          // If an error occurred...
+          if (err) {
+            console.log("Error in query: ")
+            console.log(err);
+            callback(err, null);
+          }
+
+          // Log this to the console for debugging purposes.
+          console.log("Found Enrollment in games: " + JSON.stringify(result.rows));
+
+          // (The first parameter is the error variable, so we will pass null.)
+          callback(null, result.rows);
+        });
+    }
 
 //UPDATE
 exports.start = function(id, pool, callback) {
